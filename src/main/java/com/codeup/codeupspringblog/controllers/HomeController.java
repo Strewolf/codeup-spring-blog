163a5996862ca42e31b2dfb31ce66f12f00171f5
Horace Smith <horacesmith26@gmail.com>
2023-03-24 15:02:10 -0500

    commit

--- src/main/java/com/codeup/codeupspringblog/controllers/HomeController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/HomeController.java
@@ -1,13 +1,32 @@
 package com.codeup.codeupspringblog.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-public class HomeController {
+class HelloController {
 
-    @GetMapping("/home")
-    public String welcome() {
-        return "home";
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "This is the landing page!";
     }
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+    @GetMapping("/join")
+    public String showJoinForm() {
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "join";
+    }
+
 }
+
